f9579642d2538f1206b956ce07f56604f55b6271
Andreas Hartel <ahartel@gmx.de>
2020-07-19 21:44:07 +0200

    Adding loop to agent_gym.py to train multiple matches

--- agent_gym.py
+++ agent_gym.py
@@ -4,23 +4,30 @@ from board import Board
 import random
 
 
-def main():
-    board = Board()
+def run_game_loop(board, agent1, agent2):
     game = Game(board)
-    agent1 = RandomAgent(board, use_whites=True)
-    agent2 = RandomAgent(board, use_whites=False)
     while not game.is_finished():
         dice = game.roll_dice()
         board.print()
+        print("Dice for AI1 were {}.".format(dice))
         ai_moves = agent1.move(dice)
-        print("Dice for AI1 were {}. Resulting moves: {}".format(dice, ", ".join([str(move) for move in ai_moves])))
+        print("Resulting moves: {}.".format(", ".join([str(move) for move in ai_moves])))
         game.apply(game.PLAYER1, ai_moves)
 
         dice = game.roll_dice()
         board.print()
+        print("Dice for AI2 were {}.".format(dice))
         ai_moves = agent2.move(dice)
-        print("Dice for AI2 were {}. Resulting moves: {}".format(dice, ", ".join([str(move) for move in ai_moves])))
+        print("Resulting moves: {}.".format(", ".join([str(move) for move in ai_moves])))
         game.apply(game.PLAYER2, ai_moves)
+
+
+def main():
+    for _ in range(100):
+        board = Board()
+        agent1 = RandomAgent(board, use_whites=True)
+        agent2 = RandomAgent(board, use_whites=False)
+        run_game_loop(board, agent1, agent2)
 
 
 if __name__ == '__main__':
