64b59f4687842a3b1b94b3fcffa3685b25b9ca0e
Andreas Hartel <andreas@hartel.me>
2024-04-06 14:06:09 +0200

    can simulate game and detect terminal state

--- tictactoe_mcts/src/main.rs
+++ tictactoe_mcts/src/main.rs
@@ -1,15 +1,22 @@
 use std::collections::HashMap;
+use std::fmt::{self, Debug, Formatter};
 use std::hash::Hash;
+
+use rand::Rng;
 
 fn main() {}
 
+trait Node: Eq + Hash + Clone {
+    fn possible_next_states(&self, whose_turn: Player) -> Vec<Self>;
+}
+
 #[derive(Debug)]
-struct SearchTree<N: Eq + Hash> {
+struct SearchTree<N: Node> {
     root: N,
     children: HashMap<N, Vec<N>>,
 }
 
-impl<N: Eq + Hash + Clone> SearchTree<N> {
+impl<N: Node> SearchTree<N> {
     fn new(root: N) -> SearchTree<N> {
         SearchTree {
             root,
@@ -28,6 +35,15 @@ impl<N: Eq + Hash + Clone> SearchTree<N> {
     fn add_children(&mut self, pos: &N, children: Vec<N>) {
         self.children.insert(pos.clone(), children);
     }
+
+    fn random_next_state(&self, pos: &N, whose_turn: Player) -> Option<N> {
+        let possible_states = pos.possible_next_states(whose_turn);
+        if possible_states.is_empty() {
+            return None;
+        }
+        let mut rng = rand::thread_rng();
+        Some(possible_states[rng.gen_range(0..possible_states.len())].clone())
+    }
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
@@ -36,7 +52,7 @@ enum Player {
     O,
 }
 
-#[derive(Debug, Clone, Eq, Hash, PartialEq)]
+#[derive(Clone, Eq, Hash, PartialEq)]
 struct TTTPos([Option<Player>; 9]);
 
 impl TTTPos {
@@ -53,13 +69,92 @@ impl TTTPos {
         Ok(TTTPos(pos))
     }
 
-    fn possible_moves(&self, whose_turn: Player) -> Vec<TTTPos> {
+    fn with_index_and_player(&self, idx: usize, player: Player) -> Result<TTTPos, ()> {
+        if idx >= 9 {
+            return Err(());
+        }
+        let mut pos = self.0.clone();
+        pos[idx] = Some(player);
+        Ok(TTTPos(pos))
+    }
+
+    fn is_terminal(&self) -> bool {
+        self.0
+            .iter()
+            .take(3)
+            .all(|&player| player.is_some() && player == self.0[0])
+            || self
+                .0
+                .iter()
+                .skip(3)
+                .take(3)
+                .all(|&player| player.is_some() && player == self.0[3])
+            || self
+                .0
+                .iter()
+                .skip(6)
+                .take(3)
+                .all(|&player| player.is_some() && player == self.0[6])
+            || self
+                .0
+                .iter()
+                .step_by(3)
+                .take(3)
+                .all(|&player| player.is_some() && player == self.0[0])
+            || self
+                .0
+                .iter()
+                .skip(1)
+                .step_by(3)
+                .take(3)
+                .all(|&player| player.is_some() && player == self.0[1])
+            || self
+                .0
+                .iter()
+                .skip(2)
+                .step_by(3)
+                .take(3)
+                .all(|&player| player.is_some() && player == self.0[2])
+            || self
+                .0
+                .iter()
+                .step_by(4)
+                .take(3)
+                .all(|&player| player.is_some() && player == self.0[0])
+            || self
+                .0
+                .iter()
+                .skip(2)
+                .step_by(2)
+                .take(3)
+                .all(|&player| player.is_some() && player == self.0[2])
+    }
+}
+
+impl Debug for TTTPos {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for (i, &player) in self.0.iter().enumerate() {
+            if i % 3 == 0 {
+                writeln!(f)?;
+            }
+            match player {
+                Some(Player::X) => write!(f, "X")?,
+                Some(Player::O) => write!(f, "O")?,
+                None => write!(f, ".")?,
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Node for TTTPos {
+    fn possible_next_states(&self, whose_turn: Player) -> Vec<TTTPos> {
         self.0
             .iter()
             .enumerate()
             .filter_map(|(i, &player)| {
                 if player.is_none() {
-                    Some(TTTPos::from_index_and_player(i, whose_turn).unwrap())
+                    Some(self.with_index_and_player(i, whose_turn).unwrap())
                 } else {
                     None
                 }
@@ -68,7 +163,7 @@ impl TTTPos {
     }
 }
 
-fn find_most_valuable_leaf<'a, N: Eq + Hash + Clone>(tree: &'a SearchTree<N>, pos: &'a N) -> &'a N {
+fn find_most_valuable_leaf<'a, N: Node>(tree: &'a SearchTree<N>, pos: &'a N) -> &'a N {
     let mut current_pos = pos;
     loop {
         match tree.children(current_pos) {
@@ -109,7 +204,7 @@ mod tests {
     fn can_add_children_to_tree() {
         let root_pos = TTTPos::new();
         let mut tree = SearchTree::new(root_pos.clone());
-        let new_children = root_pos.possible_moves(Player::O);
+        let new_children = root_pos.possible_next_states(Player::O);
         tree.add_children(&root_pos, new_children.clone());
         assert_eq!(tree.children(&root_pos).unwrap().len(), new_children.len());
     }
@@ -117,8 +212,11 @@ mod tests {
     #[test]
     fn all_moves_are_possible() {
         let pos = TTTPos::new();
-        let moves = pos.possible_moves(Player::X);
-        assert_eq!(moves.len(), 9);
+        let states = pos.possible_next_states(Player::X);
+        assert_eq!(states.len(), 9);
+        for state in states {
+            assert_eq!(state.0.iter().filter(|&&p| p.is_some()).count(), 1);
+        }
     }
 
     #[test]
@@ -134,8 +232,9 @@ mod tests {
             None,
             Some(Player::O),
         ]);
-        let moves = pos.possible_moves(Player::X);
-        assert_eq!(moves.len(), 1);
+        let states = pos.possible_next_states(Player::X);
+        assert_eq!(states.len(), 1);
+        assert_eq!(states[0].0.iter().filter(|&&p| p.is_some()).count(), 9)
     }
 
     #[test]
@@ -150,9 +249,92 @@ mod tests {
     fn find_most_valuable_leaf_in_tree_with_children() {
         let root_pos = TTTPos::new();
         let mut tree = SearchTree::new(root_pos.clone());
-        let new_children = root_pos.possible_moves(Player::O);
+        let new_children = root_pos.possible_next_states(Player::O);
         tree.add_children(&root_pos, new_children.clone());
         let leaf = find_most_valuable_leaf(&tree, &root_pos);
         assert_eq!(leaf, &new_children[0]);
+    }
+
+    #[test]
+    fn first_row_complete_is_terminal() {
+        let pos = TTTPos::from_slice(&[
+            Some(Player::X),
+            Some(Player::X),
+            Some(Player::X),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        ]);
+        assert!(pos.is_terminal());
+    }
+
+    #[test]
+    fn first_col_complete_is_terminal() {
+        let pos = TTTPos::from_slice(&[
+            Some(Player::X),
+            None,
+            None,
+            Some(Player::X),
+            None,
+            None,
+            Some(Player::X),
+            None,
+            None,
+        ]);
+        assert!(pos.is_terminal());
+    }
+
+    #[test]
+    fn diagonal_complete_is_terminal() {
+        let pos = TTTPos::from_slice(&[
+            Some(Player::O),
+            None,
+            None,
+            None,
+            Some(Player::O),
+            None,
+            None,
+            None,
+            Some(Player::O),
+        ]);
+        assert!(pos.is_terminal());
+    }
+
+    #[test]
+    fn anti_diagonal_complete_is_terminal() {
+        let pos = TTTPos::from_slice(&[
+            None,
+            None,
+            Some(Player::O),
+            None,
+            Some(Player::O),
+            None,
+            Some(Player::O),
+            None,
+            None,
+        ]);
+        assert!(pos.is_terminal());
+    }
+
+    #[test]
+    fn can_simulate_game() {
+        let root_pos = TTTPos::new();
+        let tree = SearchTree::new(root_pos.clone());
+        let mut current_pos = root_pos;
+        let mut whose_turn = Player::X;
+        while let Some(child) = tree.random_next_state(&current_pos, whose_turn) {
+            dbg!(&child);
+            if child.is_terminal() {
+                break;
+            }
+            current_pos = child;
+            whose_turn = match whose_turn {
+                Player::X => Player::O,
+                Player::O => Player::X,
+            };
+        }
     }
 }
